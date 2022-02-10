21989c808350aa3a01c73754329d31b2390546a6
Dibyanshu502 <99415170+Dibyanshu502@users.noreply.github.com>
2022-02-11 01:08:36 +0530

    Add files via upload

---
+++ Assignment5.c
@@ -0,0 +1,15 @@
+/*Q.5.Write a C program to enter the radius of a circle and find its diameter, circumference
+and area.*/
+
+# include <stdio.h>
+const float pi = 3.14;
+void main()
+{
+    float r,d,C,A;
+    printf("Enter the radius :");
+    scanf("%f",&r);
+    d = (r/2);
+    C = 2*pi*r;
+    A = pi*pi*r;
+    printf("Diameter is %f \nCircumference is %f \nArea is %f",d,A,C);
+}
